019d6edbe419d75ae68416b36597118c78fd19ac
Naveen Chaudhary <naveen.c131@gmail.com>
2024-05-29 22:50:10 +0530

    # Added emailer

--- ipgaevent/accounts/payment.py
+++ ipgaevent/accounts/payment.py
@@ -1,4 +1,6 @@
 import base64
+import hashlib
+from binascii import hexlify, unhexlify
 from hashlib import md5
 # import md5
 import requests
@@ -28,25 +30,31 @@ def pad(data):
 	data += chr(length)*length
 	return data
 
-def encrypt(plainText,workingKey):
-    iv = b'\x00' * 16  # Ensure the IV is in bytes
-    plainText = pad(plainText).encode('utf-8')
-    encDigest = md5(workingKey.encode('utf-8')).digest()
-    enc_cipher = AES.new(encDigest, AES.MODE_CBC, iv)
-    encryptedText = enc_cipher.encrypt(plainText)
-    encryptedText_base64 = base64.b64encode(encryptedText).decode('utf-8')
-    return encryptedText_base64
+# def encrypt(plainText,workingKey):
+#     iv = b'\x00' * 16  # Ensure the IV is in bytes
+#     plainText = pad(plainText).encode('utf-8')
+#     encDigest = md5(workingKey.encode('utf-8')).digest()
+#     enc_cipher = AES.new(encDigest, AES.MODE_CBC, iv)
+#     encryptedText = enc_cipher.encrypt(plainText)
+#     encryptedText_base64 = base64.b64encode(encryptedText).decode('utf-8')
+#     return encryptedText_base64
+
+def encrypt(plainText, workingKey):
+    iv = '\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'.encode("utf-8")
+    plainText = pad(plainText)
+    bytearrayWorkingKey = bytearray()
+    bytearrayWorkingKey.extend(map(ord, workingKey))
+    enc_cipher = AES.new(hashlib.md5(bytearrayWorkingKey).digest(), AES.MODE_CBC, iv)
+    return hexlify(enc_cipher.encrypt(plainText.encode("utf-8"))).decode('utf-8')
 
 
-
-def decrypt(cipherText,workingKey):
+def decrypt(cipherText, workingKey):
     iv = '\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'
-    decDigest = md5.new()
-    decDigest.update()
-    encryptedText = cipherText.decode('hex')
-    dec_cipher = AES.new(decDigest.digest(), AES.MODE_CBC, iv)
-    decryptedText = dec_cipher.decrypt(encryptedText)
-    return decryptedText
+    encryptedText = unhexlify(cipherText)
+    bytearrayWorkingKey = bytearray()
+    bytearrayWorkingKey.extend(map(ord, workingKey))
+    decCipher = AES.new(hashlib.md5(bytearrayWorkingKey).digest(), AES.MODE_CBC, iv)
+    return unpad(decCipher.decrypt(encryptedText).decode('utf-8'))
 
 
 def generate_ccavenue_request(data):
